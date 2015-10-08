82109fa002f92b76c271789fa10ec15c424646f1
Luis Araujo <luis_araujocaballlero@hotmail.com>
2015-10-08 00:33:49 -0600

    Menu responsive

---
+++ Menu Responvise/java.js
@@ -0,0 +1,23 @@
+var contador = 1;
+
+$(document).ready(function(){
+  $(".menu_bar").click(function(){
+    if (contador===1) {
+      $("nav").animate({
+        left:'0'
+      });
+      contador=0;
+    }else{
+      contador=1;
+      $("nav").animate({
+        left:'-100%'
+      });
+    }
+
+
+
+  });
+  $(".submenu").click(function(){
+    $(this).children(".hijos").slideToggle();
+  })
+});
